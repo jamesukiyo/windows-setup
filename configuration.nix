190cc921b6014a9a56d246640216efd1df36af80
James Plummer <jamesp2001@live.co.uk>
2025-09-04 16:39:56 +0200

    better bootstrap configuration

--- configuration.nix
+++ configuration.nix
@@ -1,7 +1,13 @@
 # THIS FILE IS USED BY THE SCRIPT TO BOOTSTRAP AND IS NOT USED FOR ANYTHING ELSE
 { config, lib, pkgs, ... }:
 {
-	nix.settings.experimental-features = [ "nix-command" "flakes" "pipe-operators" ];
+	nix.settings.experimental-features = [ "cgroups" "nix-command" "flakes" "pipe-operators" ];
+	nix.settings.max-jobs = "auto";
+	nix.settings.http-connections = 0;
+	nix.settings.flake-registry = "";
+	nix.settings.show-trace = true;
+	nix.settings.use-cgroups = true;
+	nix.channel.enable = false;
   imports = [
     <nixos-wsl/modules>
   ];
